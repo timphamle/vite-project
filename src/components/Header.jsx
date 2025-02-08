057e2af98d38ea9659a3ce557e17804862314e7c
Tim Le <tim.le@duke.edu>
2025-02-08 14:40:41 -0500

    changed name to YOLOFund in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="bg-gray-800 text-white p-4 shadow-md">
       <nav className="flex justify-between">
-        <div className="text-xl font-bold">MicroStake</div>
+        <div className="text-xl font-bold">YOLOFund</div>
         <ul className="flex space-x-4">
           <li>
             <Link to="/" className="hover:text-gray-400">Discover</Link>
